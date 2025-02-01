4c96f05d91b926ead96e510229956044548ba972
naol <naolmeseret22@gmail.com>
2025-02-02 00:10:03 +0300

    adding copilot api for pronoting

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [prompot, setPrompot] = useState("");
+  const [res, setRes] = useState("");
+  const [triger, setTriger] = useState(false);
+  async function handleleasen() {
+    const res = await fetch(
+      "https://naol.app.n8n.cloud/webhook-test/2036bb49-7094-4706-8743-63249381d532"
+    );
+    console.log(res);
+  }
+  const url = "https://copilot5.p.rapidapi.com/copilot";
+  const options = {
+    method: "POST",
+    headers: {
+      "x-rapidapi-key": "4ca82cfd70msh8633c610962ffc6p1a5a51jsn08efc1ec5f44",
+      "x-rapidapi-host": "copilot5.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      message: `${prompot}`,
+      conversation_id: null,
+      tone: "BALANCED",
+      markdown: false,
+      photo_url: null,
+    }),
+  };
+  useEffect(() => {}, [triger, setTriger]);
+  async function fectchdata() {
+    console.log("fetching musics based on your prompot ");
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+      setTriger(true);
+      setRes(result.data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
-    <div>
-      Homes
+    <div className="h-full">
+      <div className="h-[65%]   pr-20">
+        {/* {message == "" ? (
+          <span className="h-full flex justify-center items-center text-3xl font-bold">
+            😃 Please tell me the symptoms of your sickness 👨
+          </span>
+        ) : (
+          ""
+        )} */}
+        {res}
+      </div>
+
+      <div className="h-[35%]  flex justify-center items-center">
+        <textarea
+          type="text"
+          className="w-[75%] min-h-[50%] rounded-lg bg-slate-800 border-none px-4 py-2 text-white"
+          placeholder="how do you feel please tell me the symptom of your sickness"
+          value={prompot}
+          onChange={(e) => setPrompot(e.target.value)}
+        />
+        <div className="w-[20%] ml-4">
+          <Button variant="contained" onClick={fectchdata}>
+            Submit
+          </Button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,37 @@
+import { LampContainer } from "../ui/lamp";
+import { motion } from "framer-motion";
 function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="flex justify-center items-center h-full ">
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl "
+        >
+          <div className="text-center">
+            <div className="mb-5 flex justify-center items-center">
+              <img
+                src="https://media.licdn.com/dms/image/v2/D4E03AQHMnvvBrGv2_Q/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1732365765826?e=1743638400&v=beta&t=jCThKCxuxLY70wxW50NNdOWvYqmbCEaBOMAOStBOZn8"
+                className="rounded-full h-40 w-40"
+              />
+            </div>
+            <div className="text-center text-2xl font-bold">
+              <span className="text-slate-100">Name :</span>
+              <span className="text-slate-300"> Naol Mesert</span>
+            </div>
+            <div className="text-center text-2xl font-bold">
+              <span className="text-slate-100"> email :</span>
+              <span className="text-slate-300">naolmeseret22@gmail.com</span>
+            </div>
+          </div>
+        </motion.h1>
+      </LampContainer>
     </div>
   );
 }
